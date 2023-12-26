0fc613f9c7054f169e20b530b45a10add8af388a
DeanRTaylor1 <deanrtaylor1@hotmail.com>
2023-12-26 17:37:59 +0700

    [fe] add select component

--- frontend/open-source-erp/src/Components/Table/TablePagination.tsx
+++ frontend/open-source-erp/src/Components/Table/TablePagination.tsx
@@ -1,18 +1,18 @@
 import { useTheme } from "../../hooks/useTheme";
+import Select from "../select/select";
 
 interface TablePaginationProps {
     page: number; // current page number
     count: number; // total number of rows
     rowsPerPage: number; // number of rows per page
     onPageChange: (newPage: number) => void;
-    onRowsPerPageChange: (newRowsPerPage: number) => void;
+    onRowsPerPageChange: (newRowsPerPage: number | string) => void;
 }
 
 const TablePagination: React.FC<TablePaginationProps> = ({ page, count, rowsPerPage, onPageChange, onRowsPerPageChange }) => {
     const totalPages = Math.ceil(count / rowsPerPage);
 
     const { getStyles } = useTheme();
-    const selectStyle = getStyles('selectDropdown');
     const navButtonStyle = getStyles('navButton');
 
 
@@ -29,20 +29,7 @@ const TablePagination: React.FC<TablePaginationProps> = ({ page, count, rowsPerP
 
             <div className="ml-auto flex gap-4 items-center">
                 <span>{`${page + 1}-${count <= (page + 1) * rowsPerPage ? count : (page + 1) * rowsPerPage} of ${count}`}</span>
-                <div className="flex gap-2">
-                    <label className="flex gap-2 items-center justify-center">
-                        Rows per page:
-                        <select
-                            value={rowsPerPage}
-                            onChange={(e) => onRowsPerPageChange(Number(e.target.value))}
-                            className={"ml-1 " + selectStyle}
-                        >
-                            <option value={5}>5</option>
-                            <option value={10}>10</option>
-                            <option value={25}>25</option>
-                        </select>
-                    </label>
-                </div>
+                <Select onChange={onRowsPerPageChange} title="Rows per page:" value={rowsPerPage.toString()} options={["5", "10", "25"]} />
                 <button
                     onClick={handlePreviousPage}
                     disabled={page === 0}
---
+++ frontend/open-source-erp/src/Components/select/select.tsx
@@ -0,0 +1,33 @@
+import { useTheme } from "../../hooks/useTheme";
+
+interface SelectProps {
+    title: string;
+    value: string;
+    onChange: (item: string | number) => void
+    additionalClasses?: string;
+    options: string[]
+}
+const Select: React.FC<SelectProps> = ({ title, value, onChange, additionalClasses, options }) => {
+    const { getStyles } = useTheme();
+    const selectStyle = getStyles('selectDropdown');
+    return (
+        <div className="flex gap-2">
+            <label className="flex gap-2 items-center justify-center">
+                {title}
+                <select
+                    value={value}
+                    onChange={(e) => onChange(Number(e.target.value))}
+                    className={`ml-1 ${selectStyle} ${additionalClasses}`}
+                >
+                    {options.map((option) => {
+                        return (
+                            <option value={option}>{option}</option>
+                        )
+                    })}
+                </select>
+            </label>
+        </div >
+    );
+}
+
+export default Select
